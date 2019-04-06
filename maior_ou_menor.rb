b09424d6c075b83f090f9ba20a099f95da15d4f0
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-06 00:36:35 -0300

    Refatorando o codigo com os new lines

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,17 +1,14 @@
 puts "Bem vindo ao jogo da adivizinhação"
 puts "Qual o seu nome"
 nome = gets
-puts 
-puts
-puts
+
+puts "\n\n\n\n\n" 
 puts "Começaremos o jogo para você, #{nome}"
 puts "Escolhendo um numero secreto de 0 a 200..."
 numero_secreto = 175
-
 puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
+puts "\n\n\n\n" 
 
-puts 
-puts
 puts "tentaiva 1"
 puts "entre com seu numero"
 chute = gets
