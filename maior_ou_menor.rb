a3d39d206cc38a2c653a5be334940114549c01fb
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-06 16:46:33 -0300

    Adicionando numero aleatorio e pontuando o game

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,14 +1,14 @@
 def da_boas_vindas
     puts "Bem vindo ao jogo da adivizinhação"
     puts "Qual o seu nome"
-    nome = gets
+    nome = gets.strip
     puts "\n\n\n\n\n" 
     puts "Começaremos o jogo para você, #{nome}"
 end
 
 def sorteia_numero
     puts "Escolhendo um numero secreto de 0 a 200..."
-    numero_secreto = 175
+    numero_secreto = rand(200)
     puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
     puts "\n\n\n\n" 
     numero_secreto
@@ -16,9 +16,9 @@ end
 
 def pede_numero(chutes, tentativa, limite)
     puts "voce possui #{tentativa} de #{limite}"
-    puts "Chutes até agora" + chutes.to_s
+    puts "Chutes até agora #{chutes}"
     puts "\nentre com seu numero"
-    chute = gets
+    chute = gets.strip
     puts "Será que acertou? voce digitou #{chute}"
     chute.to_i
 end
@@ -41,15 +41,18 @@ end
 
 da_boas_vindas
 numero_secreto = sorteia_numero
-
+pontos_ate_agora = 1000
 limite = 5
 chutes = []
-totalChutes = 0
 
 for tentativa in 1..limite
     chute = pede_numero(chutes, tentativa, limite)
-    chutes[totalChutes] = chute
-    totalChutes+=1
+    chutes << chute
+
+    pontos_a_perder = (chute - numero_secreto).abs / 2.0
+    pontos_ate_agora -= pontos_a_perder
 
     break if verifica_numero(chute, numero_secreto)
 end
+
+puts "Você ganhou #{pontos_ate_agora} pontos"
