c5535d1da6e379fe8744bc300e917c95b87a3be9
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-06 00:59:51 -0300

    Criando funções e aprendendo escopo das variaveis

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,13 +1,21 @@
-puts "Bem vindo ao jogo da adivizinhação"
-puts "Qual o seu nome"
-nome = gets
+def da_boas_vindas
+    puts "Bem vindo ao jogo da adivizinhação"
+    puts "Qual o seu nome"
+    nome = gets
+    puts "\n\n\n\n\n" 
+    puts "Começaremos o jogo para você, #{nome}"
+end
 
-puts "\n\n\n\n\n" 
-puts "Começaremos o jogo para você, #{nome}"
-puts "Escolhendo um numero secreto de 0 a 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
-puts "\n\n\n\n" 
+def sorteia_numero
+    puts "Escolhendo um numero secreto de 0 a 200..."
+    numero_secreto = 175
+    puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
+    puts "\n\n\n\n" 
+    numero_secreto
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero
 
 limite = 5
 for tentativa in 1..limite
