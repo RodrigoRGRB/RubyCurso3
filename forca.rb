913881d3db7be3526cca1e98cb7d762915716c13
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-08 00:30:50 -0300

    Criando a função pede letra

--- forca.rb
+++ forca.rb
@@ -10,5 +10,21 @@ def sortea_palavra(nome)
     puts "Pronto já achamos a palavra secreta que possui #{palavra_secreta.size}"
 end
 
+def pede_uma_letra(chutes,tentativa, limite)
+    puts "\n\n\n\nTentativa #{tentativa} de #{limite}"
+    puts "Chutes até agora\n #{chutes}"
+    puts "Digite uma letra"
+    chute = gets.strip
+    chutes << chute
+end
+
+#defini variaveis
+tentativa = 0
+limite = 5
+chutes=[]
+
 nome = de_boas_vindas
 sortea_palavra(nome)
+chutes = pede_uma_letra tentativa, limite, chutes
+
+pede_uma_letra
