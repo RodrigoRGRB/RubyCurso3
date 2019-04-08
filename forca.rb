71055953597674b702c608d7ee2a2be880153f76
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-08 00:24:45 -0300

    Criando o sortea palavra

--- forca.rb
+++ forca.rb
@@ -4,4 +4,11 @@ def de_boas_vindas
     nome = gets.strip
 end
 
-de_boas_vindas
+def sortea_palavra(nome)
+    puts "#{nome} estamos buscando uma palavra bem dificil pra voce"
+    palavra_secreta = "pokemon"
+    puts "Pronto já achamos a palavra secreta que possui #{palavra_secreta.size}"
+end
+
+nome = de_boas_vindas
+sortea_palavra(nome)
