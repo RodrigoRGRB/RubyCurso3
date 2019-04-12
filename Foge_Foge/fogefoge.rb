121ccf9a624b3e818206d2aaf7b71a4e6a562d88
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-11 22:47:49 -0300

    Criando o le mapa

--- Foge_Foge/fogefoge.rb
+++ Foge_Foge/fogefoge.rb
@@ -1,10 +1,21 @@
 require_relative "ui"
 
-def joga nome
+def le_mapa(numero)
+    arquivo = "mapa#{numero}.txt"
+    texto = File.read arquivo
+    mapa = texto.split "\n"
+end
 
+def joga(nome)
+    mapa = le_mapa 1
+
+    while true
+        desenha(mapa)
+        direcao = pede_movimento
+    end
 end
 
 def inicia_fogefoge
     nome = da_boas_vindas
-    joga nome
+    joga(nome)
 end
--- Foge_Foge/ui.rb
+++ Foge_Foge/ui.rb
@@ -5,3 +5,13 @@ def da_boas_vindas
     puts "\n\n\n\n\n "
     nome
 end
+
+def desenha(mapa)
+    puts mapa
+    mapa
+end
+
+def pede_movimento
+    puts "para onde deseja ir"
+    movimento = gets.strip
+end
