6f7c019b2bf38e980c2e7b4fc992f1e90628a4ae
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-13 16:06:30 -0300

    Melhorando a versão completa do codigo

--- Foge_Foge/ui.rb
+++ Foge_Foge/ui.rb
@@ -1,21 +1,22 @@
 def da_boas_vindas
-    puts "Bem vindo ao foge foge"
-    puts "Qual é seu nome"
-    nome = gets.strip
-    puts "\n\n\n\n\n "
-    nome
-end
-
-def desenha(mapa)
-    puts mapa
-    mapa
+	puts "Bem vindo ao jogo do Pacman"
+	puts "Qual é o seu nome?"
+	nome = gets.strip
+	puts "\n\n\n\n\n\n"
+	puts "Começaremos o jogo para você, #{nome}"
+	nome
 end
 
 def pede_movimento
-    puts "para onde deseja ir"
-    movimento = gets.strip
+	puts "Para onde deseja ir?"
+	movimento = gets.strip
 end
 
-def gameover
-    puts "game over"
+def desenha(mapa)
+	puts mapa
+end
+
+def game_over
+	puts "\n\n\n\n\n\n"
+	puts "Game Over"
 end
