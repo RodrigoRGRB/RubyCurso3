21affd711115e6156420f9efa4fe7d784431cce0
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-13 14:03:30 -0300

    Criando a classe heroi

---
+++ Foge_Foge/heroi.rb
@@ -0,0 +1,13 @@
+class Heroi
+    attr_acessor :linha, :coluna
+
+end
+
+guilherme = Heroi.new
+guilherme.linha = 15
+guilherme.linha = 3
+
+carlos = Heroi.new
+carlos.linha = 10
+carlos.linha = 1
+
