5c41a3cd74f5a726fdf298e1205ac3b246e3981a
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-20 01:36:31 -0300

    Adicionando o formulario de ruby na unha

--- loja/loja/config/routes.rb
+++ loja/loja/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "produtos/new", to: "produtos#new"
   root to: "produtos#index"
-
 end
